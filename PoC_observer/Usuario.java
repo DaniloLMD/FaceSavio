7b5551fa07b4d23bd3e63c7b6c505c2d85e3fee2
C4I0 <107209603+caio-farias21@users.noreply.github.com>
2024-05-17 21:50:02 +0000

    poc observer

---
+++ PoC_observer/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario implements INotificavel {
+    private String nome;
+    private GerenciadorNotificacoes gerenciadorNotificacoes = new GerenciadorNotificacoes(this);
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void publicar(String msg) {
+        System.out.println("Eu " + this.nome + ", publiquei a mensagem '" + msg + "'.");
+        gerenciadorNotificacoes.notificarTodos(msg);
+    }
+
+    public void adicionarAmigo(Usuario usuario) {
+        gerenciadorNotificacoes.adicinar(usuario);
+    }
+
+    public void notificar(String msg, Usuario autor) {
+        System.out.println("Sou o usuário " + this.nome + " e o usuário " + autor.getNome() + " publicou a mensagem: '" + msg + "'.");
+    }
+
+}
